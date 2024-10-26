958af788b80f26c23791563bafdb7717db1d250b
nhinhi22jenny <167383145+nhinhi22jenny@users.noreply.github.com>
2024-10-26 14:59:32 +1100

    Update __init__.py

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+import pymysql
+pymysql.install_as_MySQLdb()
 
 db = SQLAlchemy()
 migrate = Migrate()
